2862134f254a5092080363a73b4dd7602e2e452b
Félix <felix@rockett.fr>
2023-04-12 09:14:24 +0200

    Add first game

--- scripts/level.js
+++ scripts/level.js
@@ -8,6 +8,8 @@ class LevelScene extends Phaser.Scene {
     this.load.image('bomb', 'assets/images/bomb.png');
     this.load.image('selector', 'assets/images/selector.png');
     this.load.json('levels', 'assets/data/levels.json');
+    this.load.image('cursor', 'assets/images/cursor.png');
+
   }
 
   create() {
@@ -33,11 +35,84 @@ class LevelScene extends Phaser.Scene {
     }).setOrigin(0.5);
 
     // Ajout de la bombe à désamorcer
-    let bomb = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'bomb');
+    //let bomb = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'bomb');
+
+    // Calcul de la vitesse de rotation du curseur en fonction du niveau
+    let baseRotationSpeed = 0.02; // Vitesse de rotation de base (radians par image)
+    this.cursorRotationSpeed = baseRotationSpeed * levelData.levelNumber;
     
+    // Ajout de l'écouteur d'événements pour la touche "espace"
+    let spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    let circleRadius = 100;
+
+    // Dessin du cercle au milieu du jeu
+    let circle = new Phaser.Geom.Circle(this.cameras.main.centerX, this.cameras.main.centerY, circleRadius);
+    let circleGraphics = this.add.graphics({ lineStyle: { width: 2, color: 0xffffff } });
+    circleGraphics.strokeCircleShape(circle);
+
+    // Ajout du curseur qui tourne sur les contours du cercle
+    let cursor = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'cursor');
+    cursor.setOrigin(0.5, 1); // Positionne l'origine du curseur à sa base
+
+    // 
+    this.cursor = cursor;
+
+    let angle = 0;
+
+    // Variable pour contrôler la rotation du curseur
+    let cursorRotating = true;
+
+    // Faire tourner le curseur autour du cercle
+    this.time.addEvent({
+      delay: 5,
+      loop: true,
+      callback: () => {
+        if (cursorRotating) {
+          angle += 5;
+          this.cursor.rotation = Phaser.Math.DegToRad(angle);
+        }
+      }
+    });
+
+    // Coordonnées du centre du cercle
+    let circleCenterX = this.cameras.main.centerX;
+    let circleCenterY = this.cameras.main.centerY;
+
+    // Génération d'un angle aléatoire en degrés pour positionner le rectangle
+    let rectangleAngle = Phaser.Math.Between(0, 360);
+
+    // Conversion de l'angle en radians
+    let rectangleAngleRadians = Phaser.Math.DegToRad(rectangleAngle);
+
+    // Calcul des coordonnées du rectangle en utilisant des coordonnées polaires
+    let rectangleX = circleCenterX + circleRadius * Math.cos(rectangleAngleRadians);
+    let rectangleY = circleCenterY + circleRadius * Math.sin(rectangleAngleRadians);
+
+    let rectWidth = 30;
+
+    // Ajout du rectangle avec les coordonnées calculées
+    let zoneRectangle = this.add.rectangle(rectangleX, rectangleY, 10, rectWidth, 0xffffff);
+
+    // Rotation du rectangle pour qu'il soit tangent au cercle
+    zoneRectangle.rotation = rectangleAngleRadians;
+
+    // Lorsque la touche "espace" est enfoncée, arrêtez ou démarrez la rotation du curseur
+    spaceKey.on('down', () => {
+      cursorRotating = !cursorRotating;
+
+      //console.log(this.cursor, zoneRectangle, circleRadius);
+
+      if (this.isCursorInRect(this.cursor.angle, zoneRectangle.angle, rectWidth, circleRadius)) {
+        console.log('Le curseur est dans le rectangle');
+      } else {
+        console.log('Le curseur est en dehors du rectangle');
+      }
+    });
+
     // Ajout du sélecteur pour désamorcer la bombe
-    let selector = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'selector');
-    selector.setScale(0.8);
+    //let selector = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'selector');
+    //selector.setScale(0.8);
     let codeInput = this.add.dom(this.cameras.main.centerX, this.cameras.main.centerY + 200, 'input', 'font-size: 32px; width: 200px; height: 40px; padding: 10px; border: none; border-radius: 5px; text-align: center;');
     codeInput.node.placeholder = 'Saisir le code';
 
@@ -86,5 +161,28 @@ class LevelScene extends Phaser.Scene {
           },
           callbackScope: this
         });
+      }
+
+      update () {
+
+        // Pour l'augmentation de la vitesse de rotation en fonction du niveau actuel
+        //this.cursor.rotation += this.cursorRotationSpeed;
+      }
+
+      isCursorInRect(cursorAngleTT, rectAngle, rectWidth, circleRadius) {
+
+        let wrappedRadians = Phaser.Math.Angle.Wrap(cursorAngleTT);
+        let cursorAngleInDegrees = Phaser.Math.RadToDeg(wrappedRadians);
+        let teub = Phaser.Math.Angle.WrapDegrees(cursorAngleInDegrees);
+
+        console.log(teub, rectAngle, rectWidth);
+        let circonfTotaleCercle = 2 * Math.PI * circleRadius;
+
+        let decalageDroite = ((rectWidth/2)/circonfTotaleCercle) * 360;
+        let decalageGauche = ((-rectWidth/2)/circonfTotaleCercle) * 360;
+
+        console.log(circonfTotaleCercle, rectAngle - decalageDroite, rectAngle - decalageGauche);
+
+        //return cursorAngle >= rectAngleStart && cursorAngle <= rectAngleEnd;
       }
     }
